722bab56105c0ee07aa8bd84f049827e76e8e5c5
cjswo4034 <cjswo4034@gmail.com>
2023-01-08 14:28:26 +0900

    feat - #7: Notification Controller 1차 구현
    - 구현한 핸들러
        - 알람 개수만 조회하는 핸들러
        - 알람 목록을 조회하는 핸들러
        - 개별 알람을 조회하는 핸들러
          > - 다음과 같은 상황 중 하나를 가정함
          > 1. 응답으로 이동할 url을 반환
          > 2. 이동해야될 경로로 리다이렉팅

---
+++ server/src/main/java/com/codestates/hobby/domain/notifcation/controller/NotificationController.java
@@ -0,0 +1,35 @@
+package com.codestates.hobby.domain.notifcation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+// TODO: return Status.NO_CONTENT if has no contents
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class NotificationController {
+	// 알람 개수만 조회
+	@GetMapping("/notifications/counts")
+	public ResponseEntity<?> getCounts() {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	// 알람 목록 조회
+	@GetMapping("/notifications")
+	public ResponseEntity<?> getAll() {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	// 알람 개별 조회
+	@GetMapping("/notifications/{notification-id}")
+	public ResponseEntity<?> get(@PathVariable("notification-id") long notificationId) {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
