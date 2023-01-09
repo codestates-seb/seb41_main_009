92676e3ed0edf6f64047a3279b01b06175dc9ee5
cjswo4034 <cjswo4034@gmail.com>
2023-01-09 16:36:35 +0900

    refactor - #6: Showcase Comment Controller 파라미터 추가
    - 변경사항
      - post, patch 메서드는 단일 필드를 수신하므로 content를 추가함

--- server/src/main/java/com/codestates/hobby/domain/showcase/controller/ShowcaseCommentController.java
+++ server/src/main/java/com/codestates/hobby/domain/showcase/controller/ShowcaseCommentController.java
@@ -1,5 +1,7 @@
 package com.codestates.hobby.domain.showcase.controller;
 
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,14 +25,18 @@ public class ShowcaseCommentController {
 	}
 
 	@PostMapping
-	public ResponseEntity<?> post(@PathVariable("showcase-id") long showcaseId) {
+	public ResponseEntity<?> post(
+		@PathVariable("showcase-id") long showcaseId,
+		@RequestBody @NotBlank String content
+	) {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@PatchMapping("/{comment-id}")
 	public ResponseEntity<?> patch(
 		@PathVariable("showcase-id") long showcaseId,
-		@PathVariable("comment-id") long commentId
+		@PathVariable("comment-id") long commentId,
+		@RequestBody @NotBlank String content
 	) {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
