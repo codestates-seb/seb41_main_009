45d9ae01800d1512a6b7924b884d0e0b88004913
cjswo4034 <cjswo4034@gmail.com>
2023-01-09 20:43:33 +0900

    feat - #34: Showcase Controller 2차 구현
    - 구현사항
      - Service DI
      - 핸들러 메서드 내에서 Service 메서드 호출
      - Service 메서드에서 필요한 파라미터를 DTO에 추가함

--- server/src/main/java/com/codestates/hobby/domain/showcase/controller/ShowcaseQueryController.java
+++ server/src/main/java/com/codestates/hobby/domain/showcase/controller/ShowcaseQueryController.java
@@ -1,11 +1,17 @@
 package com.codestates.hobby.domain.showcase.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codestates.hobby.domain.showcase.entity.Showcase;
+import com.codestates.hobby.domain.showcase.service.ShowcaseService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,23 +19,50 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping
 @RequiredArgsConstructor
 public class ShowcaseQueryController {
+	private final ShowcaseService showcaseService;
+
 	@GetMapping("/showcases/{showcase-id}")
-	public ResponseEntity<?> get(@PathVariable("showcase-id") long showcaseId) {
-		return new ResponseEntity<>(HttpStatus.OK);
+	public ResponseEntity<?> get(@AuthenticationPrincipal Long memberId, @PathVariable("showcase-id") long showcaseId) {
+		Showcase showcase = showcaseService.findById(showcaseId);
+
+		return new ResponseEntity<>(showcase, HttpStatus.OK);
 	}
 
 	@GetMapping("/showcases")
-	public ResponseEntity<?> getAll() {
+	public ResponseEntity<?> getAll(
+		@AuthenticationPrincipal Long memberId,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "1") int size,
+		@RequestParam(defaultValue = "NEWEST") String sort
+	) {
+		Page<Showcase> showcases = showcaseService.findAll(page, size, sort.equalsIgnoreCase("NEWEST"));
+
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@GetMapping("/categories/{category-name}/showcases")
-	public ResponseEntity<?> getAllByCategory(@PathVariable("category-name") String category) {
+	public ResponseEntity<?> getAllByCategory(
+		@AuthenticationPrincipal Long memberId,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "1") int size,
+		@RequestParam(defaultValue = "NEWEST") String sort,
+		@PathVariable("category-name") String category
+	) {
+		Page<Showcase> showcases = showcaseService.findAllByCategory(category, page, size, sort.equalsIgnoreCase("NEWEST"));
+
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@GetMapping("/members/{member-id}/showcases")
-	public ResponseEntity<?> getAllByMember(@PathVariable("member-id") long memberId) {
+	public ResponseEntity<?> getAllByMember(
+		@AuthenticationPrincipal Long authMemberId,
+		@PathVariable("member-id") long memberId,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "1") int size,
+		@RequestParam(defaultValue = "NEWEST") String sort
+	) {
+		Page<Showcase> showcases = showcaseService.findAllByMember(memberId, page, size, sort.equalsIgnoreCase("NEWEST"));
+
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
