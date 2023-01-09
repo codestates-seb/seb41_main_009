598ce3f4d4df425d72467424d572100b61204041
m0rethan <oon74326@gmail.com>
2023-01-09 21:37:23 +0900

    feat - #6: Member Dto 1차 구현 및 Controller 로그 수정

--- server/src/main/java/com/codestates/hobby/domain/member/controller/MemberController.java
+++ server/src/main/java/com/codestates/hobby/domain/member/controller/MemberController.java
@@ -14,25 +14,25 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity postMember() {
-        log.info("\n--회원 가입--\n");
+        log.info("\n\n--회원 가입--\n");
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
     public ResponseEntity patchMember(@PathVariable("member-id") long memberId) {
-        log.info("\n--회원 정보 수정--\n");
+        log.info("\n\n--회원 정보 수정--\n");
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
-        log.info("\n--회원 탈퇴--\n");
+        log.info("\n\n--회원 탈퇴--\n");
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
-        log.info("\n--해당 회원의 정보 조회--\n");
+        log.info("\n\n--해당 회원의 정보 조회--\n");
         return new ResponseEntity(HttpStatus.OK);
     }
 }
