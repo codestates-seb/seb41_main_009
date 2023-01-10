48d6ac724da85911b2de4389f856a64ae0f9b510
Gyuddi <samll8539@naver.com>
2023-01-10 13:27:32 +0900

    #6: PostCommentDto에 member SimpleResponse 추가

--- server/src/main/java/com/codestates/hobby/domain/post/dto/PostCommentDto.java
+++ server/src/main/java/com/codestates/hobby/domain/post/dto/PostCommentDto.java
@@ -12,7 +12,6 @@ public class PostCommentDto {
     @NoArgsConstructor
     public static class Response{
         private long id;
-        private long memberId;
         private String content;
         private LocalDateTime createdAt;
         private LocalDateTime lastModifiedAt;
