fbdd5b74c5171d376e4df9694e224c6b0717b656
HJeong1200 <hjeong1200@gmail.com>
2023-01-10 14:21:14 +0900

    feat - #49 : Post 생성 페이지 Button 컨테이너 분리

--- client/src/components/templates/PostCreatePage.js
+++ client/src/components/templates/PostCreatePage.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import PostCreateBody from '../organisms/postcreate/PostCreateBody';
+import PostCreateButtons from '../organisms/postcreate/PostCreateButtons';
 import PostCreateDescription from '../organisms/postcreate/PostCreateDescription';
 import PostCreateHeader from '../organisms/postcreate/PostCreateHeader';
 import PostCreateIndex from '../organisms/postcreate/PostCreateIndex';
@@ -22,6 +23,7 @@ const PostCreatePage = () => {
       <PostCreateDescription />
       <PostCreateIndex />
       <PostCreateBody />
+      <PostCreateButtons />
     </Container>
   );
 };
