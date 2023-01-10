10931997105250205aa9d8b30f7fed793f6d36ac
Atia <67799687+AtiaKR@users.noreply.github.com>
2023-01-10 12:16:10 +0900

    feat: Showcase Route 추가

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from './components/templates/Signin';
 import Navigator from './components/organisms/Navigator';
 import PostList from './components/templates/PostList';
+import Showcase from './components/templates/Showcase';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Navigator />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Showcase />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/posts" element={<PostList />} />
         </Routes>
