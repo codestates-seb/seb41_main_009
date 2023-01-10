a7964344251d0abb02573a10e2f5ba64f192d064
cjswo4034 <cjswo4034@gmail.com>
2023-01-10 12:43:00 +0900

    refactor - #33: Notification Service 1차 구현

---
+++ server/src/main/java/com/codestates/hobby/domain/notifcation/service/NotificationService.java
@@ -0,0 +1,28 @@
+package com.codestates.hobby.domain.notifcation.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.codestates.hobby.domain.notifcation.entity.Notification;
+
+@Service
+@Transactional(readOnly = true)
+public class NotificationService {
+	public long countByMember(long memberId) {
+		return 0;
+	}
+
+	public Notification findById(long notificationId) {
+		return null;
+	}
+
+	public Page<Notification> findAllByMember(long memberId, int page, int size) {
+		return null;
+	}
+
+	@Transactional
+	public void delete(long memberId, long notificationId) {
+
+	}
+}
