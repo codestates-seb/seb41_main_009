6cc8ed59f65995bb25300eb4b878913b77c382bc
cjswo4034 <cjswo4034@gmail.com>
2023-01-11 22:10:18 +0900

    feat - #73: Service 2차 구현

--- server/src/main/java/com/codestates/hobby/domain/category/service/CategoryService.java
+++ server/src/main/java/com/codestates/hobby/domain/category/service/CategoryService.java
@@ -6,27 +6,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.codestates.hobby.domain.category.entity.Category;
+import com.codestates.hobby.domain.category.repository.CategoryRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CategoryService {
+	private final CategoryRepository categoryRepository;
+
 	public Category findById(long categoryId) {
-		return null;
+		return categoryRepository.findById(categoryId)
+			.orElseThrow(() -> new IllegalArgumentException("Not found category for " + categoryId));
 	}
 
-	public Category findByName(String categoryName) {
-		return null;
+	public Category findHobbyByName(String categoryName, boolean isKorean) {
+		return (isKorean
+			? categoryRepository.findByGroupIsNotNullAndKorName(categoryName)
+			: categoryRepository.findByGroupIsNotNullAndEngName(categoryName.toLowerCase())
+		).orElseThrow(() -> new IllegalArgumentException("Not found category for " + categoryName));
+	}
+
+	public Category findGroupByName(String categoryName, boolean isKorean) {
+		return (isKorean
+			? categoryRepository.findByGroupIsNullAndKorName(categoryName)
+			: categoryRepository.findByGroupIsNullAndEngName(categoryName.toLowerCase())
+		).orElseThrow(() -> new IllegalArgumentException("Not found category for " + categoryName));
 	}
 
 	public List<Category> findAll() {
-		return null;
-	}
-
-	public List<Category> findAllByGroup(String group) {
-		return null;
+		return categoryRepository.findAll();
 	}
 
 	public List<Category> findAllGroups() {
-		return null;
+		return categoryRepository.findAllByGroupIsNull();
+	}
+
+	public List<Category> findAllByGroup(String group) {
+		return categoryRepository.findAllByGroup(findGroupByName(group, false));
 	}
 }
