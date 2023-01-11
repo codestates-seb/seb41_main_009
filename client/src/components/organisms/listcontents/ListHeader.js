9cc33cfb597a5294d3dc5a3d7a00b53d0cc3c879
HJeong1200 <hjeong1200@gmail.com>
2023-01-11 15:01:13 +0900

    feat - #63 : Header 외부 컨테이너 디자인 구현

--- client/src/components/organisms/listcontents/ListHeader.js
+++ client/src/components/organisms/listcontents/ListHeader.js
@@ -2,11 +2,19 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  width: inherit;
+  box-sizing: border-box;
+  width: 100%;
   height: 116px;
-  background-color: aqua;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 60px;
+  gap: 10px;
+  border: 5px solid var(--gray-700);
+  border-radius: 15px;
+  box-shadow: var(--boxShadow-01) var(--gray-700);
+
+  margin: 500px;
 `;
 
 const ListHeaderContainer = () => {
