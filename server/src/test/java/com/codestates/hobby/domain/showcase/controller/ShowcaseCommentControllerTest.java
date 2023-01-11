95c23d18f4b71627f85f8a2dc76fb9ce0657985b
cjswo4034 <cjswo4034@gmail.com>
2023-01-11 14:49:28 +0900

    feat - #36: Showcase, Comment 테스트 케이스 작성
    - 컨트롤러에서 자주 쓰는 어노테이션을 상속받을 수 있는 클래스 구현함

---
+++ server/src/test/java/com/codestates/hobby/domain/showcase/controller/ShowcaseCommentControllerTest.java
@@ -0,0 +1,35 @@
+package com.codestates.hobby.domain.showcase.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.codestates.hobby.domain.showcase.mapper.ShowcaseCommentMapper;
+import com.codestates.hobby.domain.showcase.service.ShowcaseCommentService;
+import com.codestates.hobby.utils.ControllerTest;
+
+@WebMvcTest(value = ShowcaseCommentController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+class ShowcaseCommentControllerTest extends ControllerTest {
+	@MockBean
+	ShowcaseCommentService service;
+
+	@MockBean
+	ShowcaseCommentMapper mapper;
+
+	@Test
+	void getAll() {
+	}
+
+	@Test
+	void post() {
+	}
+
+	@Test
+	void patch() {
+	}
+
+	@Test
+	void delete() {
+	}
+}
