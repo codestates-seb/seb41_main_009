1d77aa4e0e13dddcecb1da81b14065410969a3b8
Atia <67799687+AtiaKR@users.noreply.github.com>
2023-01-11 15:16:42 +0900

    feat - #47: 메인에 User 템플릿 연결

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Navigator from './components/organisms/Navigator';
 import PostList from './components/templates/PostList';
 import Showcase from './components/templates/Showcase';
 import Sidebar from './components/organisms/Sidebar';
+import User from './components/templates/User';
+import UserEdit from './components/templates/UserEdit';
 import SeriesList from './components/templates/SeriesList';
 import GlobalStyled from './GlobalStyle';
 import PublicRoute from './routes/PublicRoute';
@@ -26,6 +28,8 @@ const App = () => {
             <Route path="/signin" element={<PublicRoute component={<Signin />} />} />
             <Route path="/posts" element={<PublicRoute component={<PostList />} />} />
             <Route path="/series" element={<PublicRoute component={<SeriesList />} />} />
+            <Route path="/user" element={<PublicRoute component={<User />} />} />
+            <Route path="/user/edit" element={<PublicRoute component={<UserEdit />} />} />
           </Routes>
         </BrowserRouter>
       </Main>
