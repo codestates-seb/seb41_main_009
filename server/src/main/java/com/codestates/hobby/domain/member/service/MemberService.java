0800dddc306697211adfbe71d13687082d6e3f2f
m0rethan <oon74326@gmail.com>
2023-01-12 22:18:18 +0900

    feat - #73: Member Service 2차 구현

--- server/src/main/java/com/codestates/hobby/domain/member/service/MemberService.java
+++ server/src/main/java/com/codestates/hobby/domain/member/service/MemberService.java
@@ -1,28 +1,42 @@
 package com.codestates.hobby.domain.member.service;
 
+import com.codestates.hobby.domain.member.repository.MemberRepository;
 import com.codestates.hobby.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Transactional
 @Service
 public class MemberService {
+    private final MemberRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
     public Member create(Member member) {
         //email 중복 확인
-        //verifiedEmail = repository.findByEmail(member.getEmail())
+        verifyExistEmail(member.getEmail());
 
-        //nickname 중복 확인
-        //verifiedNickname = repository.findByNickname(member.getNickname())
+        //닉네임 중복 확인
+        verifyExistNickname(member.getNickname());
 
         //비밀번호 암호화
-        return member;
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+
+        //(String email, String nickname, String password, boolean isOauth2) {
+        member = new Member(member.getEmail(), member.getNickname(), member.getPassword(), member.getIntroduction(), false);
+
+        return repository.save(member);
     }
 
-    public Member edit(Long memberId, Member member) {
+    public Member edit(long memberId, Member member) {
         //멤버가 있는지 확인
-        Member findMember = findVerifiedById(memberId);
+        Member findMember = findMemberById(memberId);
         //수정을 요청한 멤버와 로그인 중인 멤버가 일치하는지 확인
         if(findMember.getId() != loginMember().getId()) {
             throw new RuntimeException("Unauthorized");
@@ -34,18 +48,18 @@ public class MemberService {
         return member;
     }
 
-    public void delete(long id) {
+    public void delete(long memberId) {
         //멤버가 있는지 확인
-        Member findMember = findVerifiedById(id);
+        Member findMember = findMemberById(memberId);
         //삭제를 요청한 멤버와 로그인 중인 멤버가 일치하는지 확인
         if(findMember.getId() != loginMember().getId()) {
             throw new RuntimeException("Unauthorized");
         }
     }
 
-    public Member findAll(long id) {
+    public Member findAll(long memberId) {
         //멤버가 있는지 확인
-        Member findMember = findVerifiedById(id);
+        Member findMember = findMemberById(memberId);
         //정보 조회를 요청한 멤버와 로그인 중인 멤버가 일치하는지 확인
         if(findMember.getId() != loginMember().getId()) {
             throw new RuntimeException("Unauthorized");
@@ -54,15 +68,29 @@ public class MemberService {
         return null;
     }
 
-    public Member findVerifiedById(long id) {
+    private void verifyExistEmail(String email) {
+        Optional<Member> member = repository.findByEmail(email);
+        if(member.isPresent()) throw new RuntimeException("Email Exists");
+    }
+
+    private void verifyExistNickname(String nickname) {
+        Optional<Member> member = repository.findByNickname(nickname);
+        if(member.isPresent()) throw new RuntimeException("Nickname Exists");
+    }
+
+    //findVerifiedById 에서 수정되었습니다.
+    public Member findMemberById(long memberId) {
+        Optional<Member> optionalMember = repository.findById(memberId);
+        Member findMember = optionalMember.orElseThrow(() -> new RuntimeException("Member Not Found"));
         //해당 id로 가입된 멤버가 있는지 확인
-        //repository.findById(id)
+
+        //repository.findById(memberId)
         //있다면 해당 member를 반환하고
         //없다면 MEMBER_NOT_FOUND
         return null;
     }
 
-    public Member findVerifiedByEmail(String email) {
+    public Member findMemberByEmail(String email) {
         //해당 email로 가입된 멤버가 있는지 확인
         //repository.findByEmail(email)
         //있다면 해당 member를 반환하고
