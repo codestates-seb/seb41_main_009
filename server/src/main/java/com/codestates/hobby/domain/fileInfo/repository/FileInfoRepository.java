f1245d86ff00aaf1fc9ddd9b3d4c9494716b5b86
cjswo4034 <cjswo4034@gmail.com>
2023-01-12 18:31:55 +0900

    refactor : FileInfo Service 리펙토링
    - 변경사항
      - SignedURL을 생성할 때 생성된 파일명이 FileInfo에 존재하는 경우 고려함

--- server/src/main/java/com/codestates/hobby/domain/fileInfo/repository/FileInfoRepository.java
+++ server/src/main/java/com/codestates/hobby/domain/fileInfo/repository/FileInfoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.codestates.hobby.domain.fileInfo.entity.FileInfo;
 
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
+	boolean existsByFileURL(String fileUrl);
 }
