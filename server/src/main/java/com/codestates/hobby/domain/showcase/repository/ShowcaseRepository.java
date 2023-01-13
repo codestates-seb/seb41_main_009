0eea8efb9e424c698735b64f680baa7988255b4b
cjswo4034 <cjswo4034@gmail.com>
2023-01-13 12:12:09 +0900

    refactor: ToOne 관계인 엔티티에 대해 Fetch join 적용

--- server/src/main/java/com/codestates/hobby/domain/showcase/repository/ShowcaseRepository.java
+++ server/src/main/java/com/codestates/hobby/domain/showcase/repository/ShowcaseRepository.java
@@ -5,11 +5,16 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.codestates.hobby.domain.category.entity.Category;
 import com.codestates.hobby.domain.showcase.entity.Showcase;
 
 public interface ShowcaseRepository extends JpaRepository<Showcase, Long> {
+	@Query("select s from Showcase s "
+		+ "join fetch s.member m "
+		+ "join fetch s.category c "
+		+ "where s.id = ?1 and m.id = ?2")
 	Optional<Showcase> findByIdAndMemberId(long showcaseId, long memberId);
 
 	Page<Showcase> findAllByMemberIdOrderByIdDesc(long memberId, Pageable pageable);
