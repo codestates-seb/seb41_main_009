a3a5d8962d140219eab1f22919f341f99ab20f4e
bakjonghyo52 <cutitout52@gmail.com>
2023-01-13 16:26:08 +0900

    feat - #92 List 컴포넌트 1차 구현

---
+++ client/src/components/molecules/list/SeriesList.js
@@ -0,0 +1,147 @@
+import styled from 'styled-components';
+import { AcrylicBase } from '../../atoms/AcrylicBase';
+import { PostListStack } from './PostList';
+import { ParagraphMedium, LabelListTitle, LabelMedium } from '../../../styles/typo';
+import { UserInfoSmall } from '../UserInfo';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 10px;
+
+  width: 1056px;
+  height: 255px;
+
+  background: url(https://unsplash.it/1920/1080/?random) rgba(45, 45, 45, 0.44);
+  border-radius: 30px;
+  overflow: hidden;
+`;
+
+const InfoLayer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0px;
+  gap: 10px;
+
+  width: fit-content;
+  height: fit-content;
+`;
+
+const SeriesInfoLayer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 0px;
+
+  width: fit-content;
+  height: fit-content;
+`;
+
+const SeriesPostNumLayer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 20px;
+
+  padding: 0px;
+  gap: 20px;
+  ${LabelMedium}
+  color: #fff;
+
+  width: fit-content;
+  height: fit-content;
+
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const ContextLayer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 0px;
+  gap: 10px;
+  width: fit-content;
+  height: fit-content;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const UserBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  gap: 20px;
+
+  width: fit-content;
+  height: fit-content;
+`;
+const Title = styled.div`
+  width: ${props => props.width || '326px'};
+  height: fit-content;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  ${LabelListTitle}
+  color: #fff;
+  &:hover {
+    color: var(--gray-100);
+  }
+`;
+const Paragraph = styled.div`
+  width: ${props => props.width || '326px'};
+  height: 42px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  ${ParagraphMedium}
+  color: #fff;
+  &:hover {
+    color: var(--gray-100);
+  }
+`;
+const SeriesList = ({ width, number = '10' }) => {
+  return (
+    <Container>
+      <AcrylicBase>
+        <InfoLayer>
+          <SeriesInfoLayer>
+            <Title width={width}>Series Name</Title>
+            <SeriesPostNumLayer>
+              <p>All Post</p>
+              <p> {number} 개</p>
+            </SeriesPostNumLayer>
+          </SeriesInfoLayer>
+          <Paragraph width={width}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim
+            velit mollit. Exercitation veniam consequat sunt nostrud amet Amet minim mollit non deserunt ullamco est sit
+            aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat
+            sunt nostrud ame.
+          </Paragraph>
+          <ContextLayer>
+            <UserBox>
+              <UserInfoSmall name="UserName" image="https://unsplash.it/1920/1080/?random" />
+            </UserBox>
+          </ContextLayer>
+        </InfoLayer>
+        <PostListStack />
+      </AcrylicBase>
+    </Container>
+  );
+};
+
+export default SeriesList;
