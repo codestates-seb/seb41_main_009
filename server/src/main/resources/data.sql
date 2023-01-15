2691f346b9dc02cc71c0cb750e9cc55a9b7224de
cjswo4034 <cjswo4034@gmail.com>
2023-01-15 12:40:38 +0900

    feat: category sql 파일 생성

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO CATEGORY(id, group_id, kor_name, eng_name)
+VALUES (default, null, '스포츠', 'sports'),
+       (default, null, '금융', 'finance'),
+       (default, null, '게임', 'game'),
+       (default, null, '사교', 'social'),
+       (default, null, '예술/문화', 'art/culture'),
+       (default, null, '창작/제작', 'creation/making');
+
+INSERT INTO CATEGORY(id, group_id, kor_name, eng_name)
+VALUES (default, 1, '야구', 'baseball'),
+       (default, 1, '축구', 'soccer'),
+       (default, 1, '농구', 'basketball'),
+       (default, 2, '주식', 'stocks'),
+       (default, 2, '부동산', 'real estate'),
+       (default, 2, '가상화폐', 'virtual currency'),
+       (default, 3, '롤', 'league of legends'),
+       (default, 3, '스타', 'starcraft'),
+       (default, 3, '보드게임', 'board game'),
+       (default, 4, '음주', 'drinking'),
+       (default, 4, '파티', 'party'),
+       (default, 4, '여행', 'travel'),
+       (default, 5, '영화', 'movie'),
+       (default, 5, '음악', 'music'),
+       (default, 5, '책', 'book'),
+       (default, 6, '공예', 'craft'),
+       (default, 6, '뜨개질', 'knitting'),
+       (default, 6, '드로잉', 'drawing');
