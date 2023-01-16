1938c1bc4afa7097359d28f69159c0e165978916
HJeong1200 <hjeong1200@gmail.com>
2023-01-16 09:28:56 +0900

    feat - #116 : 404 페이지 생성 및 App.js 연결

---
+++ client/src/components/templates/404ErrorPage.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const ErrorPage = () => {
+  return <Container />;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default ErrorPage;
