e59c6d81bdd06e79ddb7ed2a6529ffbca86ce658
HJeong1200 <hjeong1200@gmail.com>
2023-01-16 09:39:23 +0900

    feat - #116 : Buttons 생성

--- client/src/components/templates/404ErrorPage.js
+++ client/src/components/templates/404ErrorPage.js
@@ -1,7 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { DisplayLarge } from '../../styles/typo';
+import { BlackShadowButton, WhiteShadowButton } from '../atoms/Buttons';
 
 const ErrorPage = () => {
-  return <Container />;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Heading>404 Not Found</Heading>
+      <Buttons>
+        <BlackShadowButton to="/" width="170px" height="80px">
+          홈으로
+        </BlackShadowButton>
+        <WhiteShadowButton width="170px" height="80px" onClick={() => navigate(-1)}>
+          이전 페이지로
+        </WhiteShadowButton>
+      </Buttons>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -9,6 +26,19 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Heading = styled.div`
+  ${DisplayLarge}
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  width: 50%;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 export default ErrorPage;
