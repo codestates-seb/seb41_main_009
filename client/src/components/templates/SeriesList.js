8493c06c0e2f5f7199c32f6bcac1daa5c6c7050f
HJeong1200 <hjeong1200@gmail.com>
2023-01-16 10:58:37 +0900

    feat - #124 : Series Header 구현

--- client/src/components/templates/SeriesList.js
+++ client/src/components/templates/SeriesList.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import PageHeader from '../organisms/PageHeader';
-import FilterContainer from '../organisms/listcontents/ListFilter';
 import PaginationContainer from '../organisms/listcontents/ListPagination';
 import SeriesListContainer from '../organisms/listcontents/SeriesListContainer';
+import Lnb from '../organisms/Lnb';
+import { BlueShadowButton } from '../atoms/Buttons';
 
 const Container = styled.div`
   display: flex;
@@ -13,11 +14,20 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const CreateSeriesButton = styled(BlueShadowButton)`
+  width: 170px;
+  height: 44px;
+`;
+
 const SeriesList = () => {
   return (
     <Container>
-      <PageHeader />
-      <FilterContainer />
+      <PageHeader
+        headerSubTitle="Intorest In Category"
+        headerTitle="Series In Category"
+        asideHeader={<CreateSeriesButton to="/series/new">Create Series</CreateSeriesButton>}
+      />
+      <Lnb />
       <SeriesListContainer />
       <PaginationContainer />
     </Container>
