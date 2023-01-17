f3c349c9ee64ef38cadee605833b97879b6e07ce
HJeong1200 <hjeong1200@gmail.com>
2023-01-17 10:04:33 +0900

    feat - #129 : 포스트 생성 페이지 Header 타이틀 추가 및 사이즈 조정

--- client/src/components/organisms/postcreate/PostCreateHeader.js
+++ client/src/components/organisms/postcreate/PostCreateHeader.js
@@ -1,32 +1,49 @@
 import styled from 'styled-components';
 import Input from '../../atoms/Input';
 import { CATEGORIES } from '../../../constants/Categories';
+import { DisplayXSmall } from '../../../styles/typo';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: inherit;
-  height: 50px;
+  margin-bottom: 50px;
+`;
+
+const HeaderTitleContainer = styled.div`
+  ${DisplayXSmall}
+`;
+
+const HeaderSubContainer = styled.div`
+  display: flex;
+  width: 100%;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 50px;
 `;
 
 const PostCreateHeader = () => {
   return (
     <Container>
-      <Input placeholder="제목을 입력하세요" width="80%" />
-      <select name="Category">
-        <option value="카테고리를 선택하세요">카테고리를 선택하세요</option>
-        {CATEGORIES.map(([category, ...tags]) => {
-          return (
-            <optgroup label={category[1]}>
-              {tags.map(tag => {
-                return <option value={tag[1]}>{tag[1]}</option>;
-              })}
-            </optgroup>
-          );
-        })}
-      </select>
+      <HeaderTitleContainer>Title</HeaderTitleContainer>
+      <HeaderSubContainer>
+        <Input placeholder="제목을 입력하세요" width="80%" height="45px" />
+        <select name="Category">
+          <option value="카테고리를 선택하세요">카테고리를 선택하세요</option>
+          {CATEGORIES.map(([category, ...tags]) => {
+            return (
+              <optgroup key={category[1]} label={category[1]}>
+                {tags.map(tag => {
+                  return (
+                    <option key={tag[1]} value={tag[1]}>
+                      {tag[1]}
+                    </option>
+                  );
+                })}
+              </optgroup>
+            );
+          })}
+        </select>
+      </HeaderSubContainer>
     </Container>
   );
 };
