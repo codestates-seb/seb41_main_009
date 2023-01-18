c36cb0952afee42aaaab00b60ff3479096553c8d
cjswo4034 <cjswo4034@gmail.com>
2023-01-18 10:18:28 +0900

    refactor: FileInfo Entity 리펙토링
    - 생성자 변경
      - fileURL, signedURL, index만을 가지는 생성자 추가
      - static에서 연관관계 엔티티를 직접 주입받는 생성자로 변경
      - FileURL을 VO로 변경

--- server/src/main/java/com/codestates/hobby/domain/fileInfo/entity/FileInfo.java
+++ server/src/main/java/com/codestates/hobby/domain/fileInfo/entity/FileInfo.java
@@ -1,13 +1,10 @@
 package com.codestates.hobby.domain.fileInfo.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -18,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
+import com.codestates.hobby.domain.fileInfo.dto.ImageType;
 import com.codestates.hobby.domain.member.entity.Member;
 import com.codestates.hobby.domain.post.entity.Post;
 import com.codestates.hobby.domain.series.entity.Series;
@@ -26,6 +24,7 @@ import com.codestates.hobby.domain.showcase.entity.Showcase;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Entity
@@ -35,12 +34,18 @@ public class FileInfo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, updatable = false, unique = true)
-	private String fileURL;
+	@Embedded
+	private FileURL fileURL;
+
+	@Transient
+	private String signedURL;
+
+	private Integer index;
 
 	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt = LocalDateTime.now();
 
+	@Setter
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id", updatable = false)
 	private Member member;
@@ -57,86 +62,55 @@ public class FileInfo {
 	@JoinColumn(name = "showcase_id", updatable = false)
 	private Showcase showcase;
 
-	@Transient
-	private static final Pattern pattern = Pattern.compile("([\\w\\-.]+)");
-
-	@Transient
-	private final List<String> urlTokens = new ArrayList<>(5);
-
-	public FileInfo(String fileURL) {
-		this.fileURL = fileURL;
-		this.parse(fileURL);
+	public FileInfo(String fileURL, String signedURL, Integer index) {
+		this.fileURL = new FileURL(fileURL);
+		this.signedURL = signedURL;
+		this.index = index;
 	}
 
-	private FileInfo(Post post, String fileURL) {
-		this(fileURL);
+	public FileInfo(Member member, String fileURL, int index) {
+		this.fileURL = new FileURL(fileURL);
+		this.member = member;
+		this.index = index;
+	}
+
+	public FileInfo(Series series, String fileURL, int index) {
+		this.fileURL = new FileURL(fileURL);
+		this.series = series;
+		this.index = index;
+	}
+
+	public FileInfo(Post post, String fileURL, int index) {
+		this.fileURL = new FileURL(fileURL);
+		this.index = index;
 		this.post = post;
 	}
 
-
-	private FileInfo(Member member, String fileURL) {
-		this(fileURL);
-		this.member = member;
-	}
-
-	private FileInfo(Series series, String fileURL) {
-		this(fileURL);
-		this.series = series;
-	}
-
-	private FileInfo(Showcase showcase, String fileURL) {
-		this(fileURL);
+	public FileInfo(Showcase showcase, String fileURL, int index) {
+		this.fileURL = new FileURL(fileURL);
 		this.showcase = showcase;
+		this.index = index;
 	}
 
-	private void parse(String fileURL) {
-		Matcher matcher = pattern.matcher(fileURL);
-
-		while (matcher.find())
-			urlTokens.add(matcher.group());
-
-		if (urlTokens.size() != 5)
-			throw new IllegalArgumentException();
-	}
-
-	public String getBucket() {
-		return urlTokens.get(2);
-	}
-
-	public String getBasePath() {
-		return urlTokens.get(3);
-	}
-
-	public String getFilename() {
-		return urlTokens.get(4);
+	public String getToken(TOKEN token) {
+		return fileURL.getToken(token);
 	}
 
 	public String getPath() {
-		return String.join("/", urlTokens.get(3), urlTokens.get(4));
+		return fileURL.getPath();
 	}
 
-	public static FileInfo createMemberImage(Member member, String fileURL) {
-		return new FileInfo(member, fileURL);
+	public String getFileURL() {
+		return fileURL.getFileUrl();
 	}
 
-	public static FileInfo createSeriesImage(Series series, String fileURL) {
-		return new FileInfo(series, fileURL);
-	}
-
-	public static FileInfo createShowcaseImage(Showcase showcase, String fileURL) {
-		return new FileInfo(showcase, fileURL);
-	}
-
-	public static FileInfo createPostImage(Post post, String fileURL) {
-		return new FileInfo(post, fileURL);
+	public ImageType getImageType() {
+		return fileURL.getImageType();
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		if (o == null)
-			return false;
-		if (Objects.equals(getFileURL(), o.toString()))
-			return true;
+		if (o == null) return false;
 		if (!(o instanceof FileInfo))
 			return false;
 
@@ -146,5 +120,16 @@ public class FileInfo {
 	@Override
 	public int hashCode() {
 		return fileURL != null ? fileURL.hashCode() : 0;
+	}
+
+	public enum TOKEN {
+		DOMAIN(1), BUCKET(2), BASEPATH(3), FILENAME(4), EXTENSION(5), PATH(6);
+
+		@Getter
+		private final int index;
+
+		TOKEN(int index) {
+			this.index = index;
+		}
 	}
 }
