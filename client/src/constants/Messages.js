e8d6c22a467a7d2042401ee09b8efe9e625e195b
Atia <67799687+AtiaKR@users.noreply.github.com>
2023-01-25 13:37:38 +0900

    feat - #123: 업로드 초과 경고메세지 추가

--- client/src/constants/Messages.js
+++ client/src/constants/Messages.js
@@ -1,5 +1,6 @@
 const INVALIDEMAIL = '유효한 이메일을 입력해주세요.';
 const INVALIDPASSWORD = '비밀번호는 8자 이상이며, 영문, 숫자, 특수문자가 각 1개 이상 포함되어야 합니다.';
 const PASSWORDNOTMATCH = '일치하지 않는 비밀번호입니다.';
+const UPLOAD_SIZE_EXCEEDED = '업로드 가능한 크기를 초과했습니다.';
 
-export { INVALIDEMAIL, INVALIDPASSWORD, PASSWORDNOTMATCH };
+export { INVALIDEMAIL, INVALIDPASSWORD, PASSWORDNOTMATCH, UPLOAD_SIZE_EXCEEDED };
