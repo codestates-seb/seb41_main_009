f6984e9ce0de08658dd7f23f9fed260103817942
bakjonghyo52 <cutitout52@gmail.com>
2023-01-29 01:42:46 +0900

    feat - #Series Zustand currentPostId setCurrentPostId 추가

--- client/src/store/seriesStore.js
+++ client/src/store/seriesStore.js
@@ -2,10 +2,12 @@ import create from 'zustand';
 
 const useSeriesStore = create(set => ({
   isToggleBlur: true,
-
   setBlur: () => {
     set(state => ({ isToggleBlur: !state.isToggleBlur }));
   },
+
+  currentPostId: 1,
+  setCurrentPostId: currentPostId => set({ currentPostId }),
 }));
 
 export default useSeriesStore;
