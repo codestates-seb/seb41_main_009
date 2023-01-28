0cb559fc59c407b152d00f72dc823797f0bc6591
bakjonghyo52 <cutitout52@gmail.com>
2023-01-28 23:16:36 +0900

    feat - #118 Search Results 페이지 구현

--- client/src/components/pages/SearchPage.js
+++ client/src/components/pages/SearchPage.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import { useSearchParams } from 'react-router-dom';
 
 import PageHeader from '../organisms/PageHeader';
-import Lnb from '../organisms/Lnb';
+import { LnbSearch } from '../organisms/Lnb';
 import SeriesListContainer from '../organisms/listContainter/SeriesListContainer';
 import SearchResultTitle from '../organisms/search/SearchResultTitle';
 import PostListContainer from '../organisms/listContainter/PostListContainer';
 import useGetSeriesList from '../../hooks/useGetSeriesList';
 import useGetPostList from '../../hooks/useGetPostList';
+import Pagination from '../molecules/Pagination';
+import { TextButton } from '../atoms/Buttons';
 
 const Container = styled.div`
   display: flex;
@@ -15,7 +17,22 @@ const Container = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-  gap: 30px;
+`;
+
+const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
 `;
 
 // 조건 문을 통해서 하나가 켜지면 다른게 꺼지는 형태가 되도록 조건문을 작성해야함
@@ -24,19 +41,56 @@ const Search = () => {
   const curPage = searchParams.get('page');
 
   const value = searchParams.get('value');
+  const type = searchParams.get('type');
 
-  const { seriesList } = useGetSeriesList(value, curPage);
-  const { postList } = useGetPostList(value, curPage);
+  const { seriesList, seriesPageInfo } = useGetSeriesList(value, curPage);
+  const { postList, postPageInfo } = useGetPostList(value, curPage);
 
   return (
     <Container>
       <PageHeader headerSubTitle="Results for Keyword" headerTitle={`Search results ${value}`} />
       <SearchResultTitle title="검색결과" amount="120" />
-      <Lnb />
-      <SearchResultTitle title="Post" amount="60" />
-      <PostListContainer postList={postList} />
-      <SearchResultTitle title="Series" amount="60" />
-      <SeriesListContainer seriesList={seriesList} />
+      <LnbSearch value={value} />
+
+      {type && type === 'All' ? (
+        <>
+          <ResultContainer>
+            <InnerContainer>
+              <SearchResultTitle title="Posts" amount="60" />
+              <PostListContainer postList={postList} />
+            </InnerContainer>
+            <TextButton to={`/search?value=${value}&type=Posts`}>자세히 보기</TextButton>
+          </ResultContainer>
+
+          <ResultContainer>
+            <SearchResultTitle title="Series" amount="60" />
+            <SeriesListContainer seriesList={seriesList} />
+            <TextButton to={`/search?value=${value}&type=Series`}>자세히 보기</TextButton>
+          </ResultContainer>
+        </>
+      ) : (
+        ''
+      )}
+
+      {type && type === 'Posts' ? (
+        <>
+          <SearchResultTitle title="Post" amount="60" />
+          <PostListContainer postList={postList} />
+          <Pagination totalPages={postPageInfo.totalPages} />
+        </>
+      ) : (
+        ''
+      )}
+
+      {type && type === 'Series' ? (
+        <>
+          <SearchResultTitle title="Series" amount="60" />
+          <SeriesListContainer seriesList={seriesList} />
+          <Pagination totalPages={seriesPageInfo.totalPages} />
+        </>
+      ) : (
+        ''
+      )}
     </Container>
   );
 };
