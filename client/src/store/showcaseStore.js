6cd02a124b9aa55de03c3994d6ffa09434ca68ea
Atia <67799687+AtiaKR@users.noreply.github.com>
2023-01-30 16:49:29 +0900

    fix: Showcase getItemList 버그수정

--- client/src/store/showcaseStore.js
+++ client/src/store/showcaseStore.js
@@ -6,44 +6,54 @@ const useShowcaseStore = create((set, get) => ({
   disabledFlag: false,
   setDisabledFlag: boolean => set({ disabledFlag: boolean }),
 
-  offset: 0,
+  offset: -1,
+  setOffset: offset => set({ offset }),
+
   isLoading: false,
   error: null,
   itemList: [],
   getItemList: async (count, callback) => {
-    const { disabledFlag } = get();
+    const { disabledFlag, offset, isLoading } = get();
+    const body = {
+      params: {
+        offset,
+        size: count,
+      },
+    };
+    // 중복 실행 방지
+    if (isLoading) return;
 
     // 더이상 불러올게 없으면 callback 실행 후 리턴
     if (disabledFlag === true) {
       console.log('disabled flag is on');
-      callback();
       return;
     }
 
     try {
       set({ isLoading: true });
-      const response = await axios.get('/showcases', {
-        params: {
-          offset: get().offset,
-          size: count,
-        },
-      });
+      const response = await axios.get('/showcases', body);
+      console.log(response);
 
       const { data, pageInfo } = response.data;
+      console.log(data);
+      console.log(pageInfo);
+
+      // for Offset-based Pagination
+      // response.data 의 맨마지막 요소의 id 를 offset 으로 설정
+      const newOffset = data.slice(-1).id;
+
+      // itemList 업데이트
+      set(state => ({ itemList: state.itemList.concat(...data), offset: newOffset }));
+
+      console.log(`newOffset: ${newOffset}`);
+      console.log(`itemList: ${get().itemList}`);
 
       // 더 이상 불러올 쇼케이스가 없는경우
-      if (!pageInfo.hasNext) {
+      if (pageInfo.hasNext === false) {
         set({ disabledFlag: true, isLoading: false });
         callback();
         return;
       }
-
-      // for Offset-based Pagination
-      // response.data 의 맨마지막 요소의 id 를 offset 으로 설정
-      const offset = data.slice(-1).id;
-
-      // itemList 업데이트
-      set(state => ({ itemList: state.itemList.concat(...data), offset }));
     } catch (err) {
       set({ error: err.response, disabledFlag: true });
       console.log(err.response);
