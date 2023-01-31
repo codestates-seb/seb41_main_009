dde382219b0d3a7379f935f92d29601be6f6c81c
bakjonghyo52 <cutitout52@gmail.com>
2023-01-31 18:18:08 +0900

    feat - #170  Footer 링크연결

--- client/src/components/organisms/Footer.js
+++ client/src/components/organisms/Footer.js
@@ -3,7 +3,7 @@ import { LabelMedium } from '../../styles/typo';
 import { TextButton } from '../atoms/Buttons';
 
 const Container = styled.div`
-  width: 100%;
+  width: auto;
   height: fit-content;
   background-color: var(--gray-700);
   display: flex;
@@ -17,7 +17,7 @@ const Container = styled.div`
 
 const Body = styled.div`
   display: flex;
-  width: 1440px;
+  width: 100%;
   justify-content: space-between;
   align-items: center;
 `;
@@ -60,10 +60,40 @@ const ColumnList = styled.div`
   justify-content: space-between;
 `;
 
+const ColumnList2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+  margin-left: 400px;
+  justify-content: space-between;
+`;
+const OuterColumnList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 20px;
+`;
+
 const UserButton = styled(TextButton)`
-  width: 70px;
+  width: fit-content;
   justify-content: center;
   color: var(--gray-100);
+  margin-right: 30px;
+
+  &:visited {
+    color: var(--gray-200);
+  }
+
+  ${LabelMedium}
+`;
+const Label = styled(TextButton)`
+  width: 120px;
+  justify-content: center;
+  color: var(--gray-100);
+  margin-right: 20px;
 
   &:visited {
     color: var(--gray-200);
@@ -72,27 +102,72 @@ const UserButton = styled(TextButton)`
   ${LabelMedium}
 `;
 
+const Border = styled.div`
+  width: 100%;
+  border: 1px solid #757575;
+  margin-right: 15px;
+`;
+
 const Footer = () => {
   return (
     <Container>
       <Body>
-        <ColumnList>
+        <OuterColumnList>
+          <RowList>
+            <ColumnList2>
+              <RowListFitContent>
+                <Label>client</Label>
+                <UserButton
+                  onClick={() =>
+                    window.open('https://www.notion.so/variants00/Variants-9820752a46004fcdad852b24d94f10d9', '_blank')
+                  }>
+                  bakjonghyo52
+                </UserButton>
+                <UserButton onClick={() => window.open('https://github.com/atiakr', '_blank')}>atiakr</UserButton>
+                <UserButton onClick={() => window.open('https://github.com/HJeong1200', '_blank')}>
+                  HJeong1200
+                </UserButton>
+              </RowListFitContent>
+              <RowListFitContent>
+                <Label>server</Label>
+                <UserButton onClick={() => window.open('https://github.com/cjswo4034', '_blank')}>cjswo4034</UserButton>
+                <UserButton onClick={() => window.open('https://github.com/m0rethan', '_blank')}>m0rethan</UserButton>
+                <UserButton onClick={() => window.open('https://github.com/gyuddi', '_blank')}>gyuddi</UserButton>
+              </RowListFitContent>
+            </ColumnList2>
+            <ColumnList>
+              <Label>Content Team</Label>
+              <RowListFitContent>
+                <UserButton
+                  onClick={() => window.open('https://github.com/codestates-seb/seb41_main_009/tree/README', '_blank')}>
+                  Github
+                </UserButton>
+                <UserButton
+                  onClick={() =>
+                    window.open('https://codestates.notion.site/8fe2c265d13545ad8c15bd4ea7923bbf', '_blank')
+                  }>
+                  Notion
+                </UserButton>
+              </RowListFitContent>
+            </ColumnList>
+          </RowList>
+          <Border />
           <RowList>
             <Logo>IntoRest</Logo>
             <ColumnList>
               <UserButton>Move To</UserButton>
               <RowListFitContent>
-                <UserButton to="/">login</UserButton>
-                <UserButton to="/">signup</UserButton>
-                <UserButton to="/">showcase</UserButton>
-                <UserButton to="/">posts</UserButton>
-                <UserButton to="/">series</UserButton>
-                <UserButton to="/">users</UserButton>
-                <UserButton to="/">search</UserButton>
+                <UserButton to="/login">login</UserButton>
+                <UserButton to="/signup">signup</UserButton>
+                <UserButton to="/showcase/new">showcase</UserButton>
+                <UserButton to="/posts">posts</UserButton>
+                <UserButton to="/series">series</UserButton>
+                <UserButton to="/users/1">users</UserButton>
+                <UserButton to="/search/search">search</UserButton>
               </RowListFitContent>
             </ColumnList>
           </RowList>
-        </ColumnList>
+        </OuterColumnList>
       </Body>
     </Container>
   );
