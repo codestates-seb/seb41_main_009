ad60e6d6c1ec1e8048b7cb627f49df3f6ae8d00f
Atia <67799687+AtiaKR@users.noreply.github.com>
2023-02-01 10:08:13 +0900

    chore: 콘솔로그 제거

--- client/src/store/showcaseStore.js
+++ client/src/store/showcaseStore.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import create from 'zustand';
-import { TEST_SHOWCASE_LIST_RES } from '../tests/showcaseDummy';
 
 const useShowcaseStore = create((set, get) => ({
   disabledFlag: false,
@@ -12,6 +11,7 @@ const useShowcaseStore = create((set, get) => ({
   isLoading: false,
   error: null,
   itemList: [],
+
   getItemList: async (count, callback) => {
     const { disabledFlag, offset, isLoading } = get();
     const body = {
@@ -32,21 +32,15 @@ const useShowcaseStore = create((set, get) => ({
     try {
       set({ isLoading: true });
       const response = await axios.get('/showcases', body);
-      console.log(response);
 
       const { data, pageInfo } = response.data;
-      console.log(data);
-      console.log(pageInfo);
 
       // for Offset-based Pagination
       // response.data 의 맨마지막 요소의 id 를 offset 으로 설정
-      const newOffset = data.slice(-1).id;
+      const newOffset = data.slice(-1)[0].id;
 
       // itemList 업데이트
       set(state => ({ itemList: state.itemList.concat(...data), offset: newOffset }));
-
-      console.log(`newOffset: ${newOffset}`);
-      console.log(`itemList: ${get().itemList}`);
 
       // 더 이상 불러올 쇼케이스가 없는경우
       if (pageInfo.hasNext === false) {
@@ -56,38 +50,25 @@ const useShowcaseStore = create((set, get) => ({
       }
     } catch (err) {
       set({ error: err.response, disabledFlag: true });
-      console.log(err.response);
     }
     set({ isLoading: false });
-  },
-  // TODO: 2차 구현시 지우기
-  // 무한스크롤 테스트용 함수
-  getItemListForTest: async () => {
-    try {
-      set({ isLoading: true });
-      set(state => ({
-        offset: state.offset + 6,
-        itemList: state.itemList.concat(
-          new Array(6).fill({}).map((_, idx) => {
-            return {
-              ...TEST_SHOWCASE_LIST_RES,
-              id: state.itemList.length + idx,
-            };
-          }),
-        ),
-      }));
-    } catch (err) {
-      set({ error: err.response });
-      console.log(err.response);
-    }
-    set({ isLoading: false });
-    console.log(get().itemList);
   },
 
-  postComment: async (id, contents) => {
+  postComment: async (id, content, handleInit) => {
     try {
-      await axios.post(`/showcases/${id}/comments`, contents);
+      const response = await axios.post(
+        `/showcases/${id}/comments`,
+        { content },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: false,
+        },
+      );
       console.log('코멘트를 달았습니다.');
+      console.log(response);
+      handleInit();
     } catch (err) {
       console.log(err);
     }
