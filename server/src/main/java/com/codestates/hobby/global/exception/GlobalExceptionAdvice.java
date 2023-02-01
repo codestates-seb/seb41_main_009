80435c233160ad7c6f3ee2806933cb30f06bda65
cjswo4034 <cjswo4034@gmail.com>
2023-02-01 19:40:50 +0900

    refactor: ExceptionAdvice Log level을 warn으로 변경

--- server/src/main/java/com/codestates/hobby/global/exception/GlobalExceptionAdvice.java
+++ server/src/main/java/com/codestates/hobby/global/exception/GlobalExceptionAdvice.java
@@ -26,12 +26,14 @@ public class GlobalExceptionAdvice {
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorResponse NotValidException(MethodArgumentNotValidException e) {
+		log.warn("MethodArgumentNotValidException occurred: {}", e.getMessage(), e);
 		return ErrorResponse.of(e.getBindingResult());
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorResponse ConstraintViolationException(ConstraintViolationException e) {
+		log.warn("ConstraintViolationException occurred: {}", e.getMessage(), e);
 		return ErrorResponse.of(e.getConstraintViolations());
 	}
 
@@ -41,27 +43,27 @@ public class GlobalExceptionAdvice {
 		MissingServletRequestParameterException.class, MultipartException.class,
 	})
 	public ErrorResponse handleBadRequestException(Exception e) {
-		log.debug("Bad request exception occurred: {}", e.getMessage(), e);
+		log.warn("Bad request exception occurred: {}", e.getMessage(), e);
 		return ErrorResponse.of(HttpStatus.BAD_REQUEST, e.getMessage());
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 	public ErrorResponse handleMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-		log.debug("MediaTypeNotSupportedException: {}", e.getMessage(), e);
+		log.warn("MediaTypeNotSupportedException: {}", e.getMessage(), e);
 		return ErrorResponse.of(HttpStatus.UNSUPPORTED_MEDIA_TYPE, e.getMessage());
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 	public ErrorResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-		log.debug("Request method not supported exception: {}", e.getMessage(), e);
+		log.warn("Request method not supported exception: {}", e.getMessage(), e);
 		return ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
 	}
 
 	@ExceptionHandler
 	public ResponseEntity<?> handleBusinessLogicException(BusinessLogicException e) {
-		log.debug("Business logic exception ocurred: {}", e.getMessage());
+		log.warn("Business logic exception occurred: {}", e.getMessage(), e);
 		HttpStatus status = HttpStatus.valueOf(e.getExceptionCode().getStatus());
 		return new ResponseEntity<>(ErrorResponse.of(status, e.getExceptionCode().getMessage()), status);
 	}
@@ -69,7 +71,7 @@ public class GlobalExceptionAdvice {
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ErrorResponse handleException(Exception e) {
-		log.error("# handle Exception", e);
+		log.error("Exception occurred: {}", e.getMessage(), e);
 		return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
 	}
 }
