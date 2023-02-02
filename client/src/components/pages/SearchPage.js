ff547fe826be91b01ef30303884ca159f96269e6
bakjonghyo52 <cutitout52@gmail.com>
2023-02-03 00:03:20 +0900

    refactor - SearchPage

--- client/src/components/pages/SearchPage.js
+++ client/src/components/pages/SearchPage.js
@@ -8,8 +8,8 @@ import Pagination from '../molecules/Pagination';
 import SeriesListContainer from '../organisms/listContainter/SeriesListContainer';
 import SearchResultTitle from '../organisms/search/SearchResultTitle';
 import PostListContainer from '../organisms/listContainter/PostListContainer';
-import useGetSeriesList from '../../hooks/useGetSeriesList';
-import useGetPostList from '../../hooks/useGetPostList';
+import useSearchSeriesList from '../../hooks/useSearchSeriesList';
+import useSearchPostList from '../../hooks/useSearchPostList';
 
 const Container = styled.div`
   display: flex;
@@ -36,44 +36,56 @@ const InnerContainer = styled.div`
   align-items: flex-end;
 `;
 
+const LNBGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+`;
+
 // 조건 문을 통해서 하나가 켜지면 다른게 꺼지는 형태가 되도록 조건문을 작성해야함
 const Search = () => {
-  const { keyword } = useParams();
+  const { type } = useParams();
 
   const [searchParams] = useSearchParams();
   const curPage = searchParams.get('page');
-  const type = searchParams.get('type');
+  const query = searchParams.get('query');
 
-  const { seriesList, seriesPageInfo } = useGetSeriesList(keyword, curPage);
-  const { postList, postPageInfo } = useGetPostList(keyword, curPage);
+  const { seriesList, seriesPageInfo } = useSearchSeriesList(query, curPage);
+  const { postList, postPageInfo } = useSearchPostList(query, curPage);
 
   return (
     <Container>
-      <PageHeader headerSubTitle="Results for Keyword" headerTitle={`Search results ${keyword}`} />
-      <SearchResultTitle title="검색결과" amount="120" />
-      <LnbSearch keyword={keyword} />
-
-      {type && type === 'All' ? (
+      <PageHeader headerSubTitle="Results for Keyword" headerTitle={`Search results ${query}`} />
+      <LNBGroup>
+        <SearchResultTitle
+          title="검색결과"
+          amount={Number(postPageInfo?.totalElements) + Number(seriesPageInfo?.totalElements) || 0}
+        />
+        <LnbSearch query={query} />
+      </LNBGroup>
+      {type && type === 'all' ? (
         <>
           <ResultContainer>
             <InnerContainer>
-              <SearchResultTitle title="Posts" amount="60" />
+              <SearchResultTitle title="Posts" amount={postPageInfo?.totalElements || 0} />
               <PostListContainer postList={postList} />
             </InnerContainer>
-            <TextButton to={`/search/${keyword}?type=Posts`}>자세히 보기</TextButton>
+            <TextButton to={`/search/posts?query=${query}&page=1&size=10`}>자세히 보기</TextButton>
           </ResultContainer>
 
           <ResultContainer>
-            <SearchResultTitle title="Series" amount="60" />
+            <SearchResultTitle title="Series" amount={seriesPageInfo?.totalElements || 0} />
             <SeriesListContainer seriesList={seriesList} />
-            <TextButton to={`/search/${keyword}?type=Series`}>자세히 보기</TextButton>
+            <TextButton to={`/search/series?query=${query}&page=1&size=10`}>자세히 보기</TextButton>
           </ResultContainer>
         </>
       ) : (
         ''
       )}
 
-      {type && type === 'Posts' ? (
+      {type && type === 'posts' ? (
         <>
           <SearchResultTitle title="Post" amount="60" />
           <PostListContainer postList={postList} />
@@ -83,7 +95,7 @@ const Search = () => {
         ''
       )}
 
-      {type && type === 'Series' ? (
+      {type && type === 'series' ? (
         <>
           <SearchResultTitle title="Series" amount="60" />
           <SeriesListContainer seriesList={seriesList} />
